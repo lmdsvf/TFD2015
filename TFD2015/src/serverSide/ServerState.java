c047a0a54696a6c4b003fed67130c6fd8e44bb72
Miguel <lmdsvf@gmail.com>
2015-10-19 13:42:00 +0100

    Ip change 2

--- TFD2015/src/serverSide/ServerState.java
+++ TFD2015/src/serverSide/ServerState.java
@@ -20,7 +20,7 @@ public class ServerState {
 		configuration = new ArrayList<String>();
 		configuration.add("193.137.143.40");
 		configuration.add("193.137.142.16");
-		//configuration.add("10.101.149.41");
+		//configuration.add("10.101.149.41"); 
 		replica_number = configuration.indexOf("193.137.143.40");
 		status = Status.NORMAL;
 		log = new ArrayList<ClientMessage>();
