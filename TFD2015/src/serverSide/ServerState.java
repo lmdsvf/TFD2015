e7c533cdd5a6cf846146f9f5e6b8b384431faf41
Miguel <lmdsvf@gmail.com>
2015-10-19 14:02:24 +0100

    inetaddress

--- TFD2015/src/serverSide/ServerState.java
+++ TFD2015/src/serverSide/ServerState.java
@@ -1,5 +1,7 @@
 package serverSide;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -12,18 +14,27 @@ public class ServerState {
 	private int view_number = 0;
 	private Status status;
 	private int op_number = 0;
-	private ArrayList<ClientMessage> log; //ver depois o tipo...
+	private ArrayList<ClientMessage> log; // ver depois o tipo...
 	private int commit_number;
 	private HashMap<Client, Tuple> clientTable;
-	
-	public ServerState(){
-		configuration = new ArrayList<String>();
-		configuration.add("193.137.143.40");
-		configuration.add("193.137.142.16");
-		//configuration.add("10.101.149.41"); 
-		replica_number = configuration.indexOf("193.137.143.40");
-		status = Status.NORMAL;
-		log = new ArrayList<ClientMessage>();
-		clientTable = new HashMap<Client, Tuple>(); 
+
+	public ServerState() {
+
+		try {
+			configuration = new ArrayList<String>();
+			configuration.add("193.137.143.40");
+			configuration.add("193.137.142.16");
+			// configuration.add("10.101.149.41");
+			configuration.sort(null);
+			replica_number = configuration.indexOf(InetAddress.getLocalHost()
+					.getHostAddress().toString());
+			status = Status.NORMAL;
+			log = new ArrayList<ClientMessage>();
+			clientTable = new HashMap<Client, Tuple>();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
