18bc9e8c2edf584790723398928d76e5836bf6af
Pedro Man <pedro.man023@gmail.com>
2015-10-22 02:20:27 +0100

    Classe Network ainda por refinar

--- TFD2015/src/network/Network.java
+++ TFD2015/src/network/Network.java
@@ -1,28 +1,75 @@
 package network;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Network {
 	
-	private byte[] sendData;
-	private byte[] receiveData;
-	private final int BUFFERSIZE = 1024; 
+	private byte[] receivedData;
+	private final int BUFFERSIZE = 1024;
 	private int port;
-	private InetAddress address;
-	private DataGram
+	private InetAddress IPaddress;
+	private DatagramSocket socket;
 	
+	// client constructor
 	public Network(String address, int port){
-		sendData = new byte[BUFFERSIZE];
-		receiveData = new byte[BUFFERSIZE];
-		address = new InetAddress(address,port);
+		receivedData = new byte[BUFFERSIZE];
+		this.port = port;
+		
+		try {
+			IPaddress = InetAddress.getByName(address);
+			socket = new DatagramSocket();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	// server constructor
+	public Network(int port){
+		receivedData = new byte[BUFFERSIZE];
+		this.port = port;
+		
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void send(){
-		
+	public void send(byte[] data){
+		try {
+			DatagramPacket sendPacket = new DatagramPacket(data, data.length, IPaddress, port);
+			socket.send(sendPacket);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void receive(){
+	public byte[] receive(){
+		System.out.println("Receiving data...");
+		DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
+		try {
+			socket.receive(receivedPacket);
+		} catch (IOException e) {
+			return null;
+		}
 		
+		if(IPaddress == null || port == 0){
+			IPaddress = receivedPacket.getAddress();
+			port = receivedPacket.getPort();
+		}
+		
+		System.out.println("IP: " + IPaddress.toString() + " data: " + new String(receivedData));
+		
+		return receivedData;
 	}
 }
