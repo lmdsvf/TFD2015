ac924e5d90fd188876c806c7eb2eb8f1dd830ac5
Miguel <lmdsvf@gmail.com>
2015-10-25 08:19:00 +0000

    testes!

--- TFD2015/src/clientSide/clienttest.java
+++ TFD2015/src/clientSide/clienttest.java
@@ -1,11 +1,7 @@
 package clientSide;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Properties;
 
 import message.Message;
 import message.MessageType;
@@ -14,18 +10,8 @@ import network.Network;
 public class clienttest {
 
 	public static void main(String[] args) {
-		Properties properties = new Properties();
-		try {
-			properties.load(new FileReader("Configuration.txt"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Network net = new Network("192.168.1.3", Integer.parseInt(properties
-				.getProperty("PClient")));
+
+		Network net = new Network("192.168.1.3", 4567);
 		for (int i = 0; i < 1; i++) {
 			Message a;
 			try {
--- TFD2015/src/serverSide/StartServicingClient.java
+++ TFD2015/src/serverSide/StartServicingClient.java
@@ -1,10 +1,6 @@
 package serverSide;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.SocketException;
-import java.util.Properties;
 
 import network.Network;
 import message.Message;
@@ -21,19 +17,8 @@ public class StartServicingClient extends Thread {
 
 	@Override
 	public void run() {
-		// new Network(Server.PORT);
-		Properties properties = new Properties();
-		try {
-			properties.load(new FileReader("Configuration.txt"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		server = new Network(
-				Integer.parseInt(properties.getProperty("PClient")));
+		
+		server = new Network(Server.PORT);
 		System.out.println("ServerSocket activa");
 		while (true) { // espera q venha clients
 			System.out.println("Waiting for clients...");
@@ -55,47 +40,47 @@ public class StartServicingClient extends Thread {
 
 		@Override
 		public void run() {
-			// if (server.getSocket().isConnected()) {
+			//if (server.getSocket().isConnected()) {
 
-			switch (msg.getType()) {
-			case REQUEST:
-				// send prepare to all backups
-				// Message sm = new Message(MessageType.PREPARE, 1, msg, 1,
-				// 0);
-				// for (DealWithServers ds : backupServers.values()) {
-				// ds.getNetwork().send(sm);
-				// }
+				switch (msg.getType()) {
+				case REQUEST:
+					// send prepare to all backups
+					// Message sm = new Message(MessageType.PREPARE, 1, msg, 1,
+					// 0);
+					// for (DealWithServers ds : backupServers.values()) {
+					// ds.getNetwork().send(sm);
+					// }
 
-				// wait for half the prepare_ok message
-				// int ok = 0;
-				// while(ok < (backupServers.size()/2) + 1){
-				//
-				// ok++;
-				// }
-				Message reply = new Message(MessageType.REPLY, 0,
-						msg.getRequest_Number(), "result");
-				server.send(reply);
-				break;
-			default:
-				break;
-			}
-			/*
-			 * ServerMessage message = (ServerMessage) in.readObject();
-			 * 
-			 * switch (message.getType()) { case CONNECTED: if
-			 * (!maps.contains(message.getSenderNickname())) { clientName =
-			 * message.getSenderNickname();
-			 * 
-			 * maps.addEntry(message.getSenderNickname(), out); q.add(message);
-			 * } else { ClientMessage msg = new ClientMessage(
-			 * ClientSideMessage.Type.ERROR, "ja existe esse nome");
-			 * out.writeObject(msg); } break; case NEW_ORDER: q.add(message);
-			 * break; default: break; }
-			 */
-			// } else {
-			// System.out.println("Fixe, resultou!");
-			//
-			// }
+					// wait for half the prepare_ok message
+					// int ok = 0;
+					// while(ok < (backupServers.size()/2) + 1){
+					//
+					// ok++;
+					// }
+					Message reply = new Message(MessageType.REPLY, 0,
+							msg.getRequest_Number(), "result");
+					server.send(reply);
+					break;
+				default:
+					break;
+				}
+				/*
+				 * ServerMessage message = (ServerMessage) in.readObject();
+				 * 
+				 * switch (message.getType()) { case CONNECTED: if
+				 * (!maps.contains(message.getSenderNickname())) { clientName =
+				 * message.getSenderNickname();
+				 * 
+				 * maps.addEntry(message.getSenderNickname(), out);
+				 * q.add(message); } else { ClientMessage msg = new
+				 * ClientMessage( ClientSideMessage.Type.ERROR,
+				 * "ja existe esse nome"); out.writeObject(msg); } break; case
+				 * NEW_ORDER: q.add(message); break; default: break; }
+				 */
+//			} else {
+//				System.out.println("Fixe, resultou!");
+//
+//			}
 		}
 	}
 }
