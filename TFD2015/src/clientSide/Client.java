480eb3c5eeb8e2c226b37dd384e3611749958247
PedroMan <pedro.man023@gmail.com>
2015-10-28 10:35:49 +0000

    Client stuff

--- TFD2015/src/clientSide/Client.java
+++ TFD2015/src/clientSide/Client.java
@@ -1,34 +1,27 @@
 package clientSide;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Properties;
 
-import javax.swing.JOptionPane;
-
-import message.ClientMessage;
+import network.Network;
+import message.Message;
 import message.MessageType;
-import message.Operation;
-import serverSide.Server;
 
 public class Client {
 	private ClientState state;
-	private Socket socket;
-	private ObjectOutputStream out;
-	private ObjectInputStream in;
 	private String serverAddress;
+	private int port;
 
-	public Client() {
+	public Client(int id) {
 		readConfiguration();
+		state = new ClientState(id);
+		execute();
 		// TODO Auto-generated constructor stub
-		serverAddress = JOptionPane.showInputDialog("IP").toString();
 		// connection();
 	}
 
@@ -36,7 +29,9 @@ public class Client {
 		Properties properties = new Properties();
 		try {
 			properties.load(new FileReader("Configuration.txt"));
-			System.out.println(properties.getProperty("IP0"));
+			serverAddress = properties.getProperty("IP0");
+			port = Integer.parseInt(properties.getProperty("PClient"));
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,61 +40,79 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-
-	public void connection() {
+	
+	public void execute(){
+		System.out.println(state.toString());
+		Network net = new Network(serverAddress,port);
+		Message msg = new Message(MessageType.REQUEST,"operation",state.getId()+"",0);
 		try {
-			socket = new Socket(serverAddress, Server.PORT);
-			in = new ObjectInputStream(socket.getInputStream());
-			out = new ObjectOutputStream(socket.getOutputStream());
-			/*
-			 * ServerMessage msg = new ServerMessage(
-			 * ServerSideMessage.Type.CONNECTED, nickname, null);
-			 */
-			ClientMessage msg = new ClientMessage(MessageType.REQUEST,
-					new Operation(2, 3, OperationType.SUM), 1, 1);
-			out.writeObject(msg);
-			new Receive(in, out).start();
-			// isConnected = true;
+			net.send(msg, InetAddress.getByName(serverAddress), port);
+			DatagramPacket data = net.receive();
+			Message reply = Network.networkToMessage(data);
+			System.out.println(reply.getResult());
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 
-	class Receive extends Thread {
+//	public void connection() {
+//		try {
+//			socket = new Socket(serverAddress, Server.PORT);
+//			in = new ObjectInputStream(socket.getInputStream());
+//			out = new ObjectOutputStream(socket.getOutputStream());
+//			/*
+//			 * ServerMessage msg = new ServerMessage(
+//			 * ServerSideMessage.Type.CONNECTED, nickname, null);
+//			 */
+//			ClientMessage msg = new ClientMessage(MessageType.REQUEST,
+//					new Operation(2, 3, OperationType.SUM), 1, 1);
+//			out.writeObject(msg);
+//			new Receive(in, out).start();
+//			// isConnected = true;
+//		} catch (UnknownHostException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 
-		private ObjectInputStream in;
-		private ObjectOutputStream out;
-
-		public Receive(ObjectInputStream in, ObjectOutputStream out) {
-			this.in = in;
-			this.out = out;
-		}
-
-		@Override
-		public void run() {
-			try {
-				while (true) {
-					ClientMessage msg = (ClientMessage) in.readObject();
-					System.out.println("Resultado: " + msg.getX());
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-	}
+//	class Receive extends Thread {
+//
+//		private ObjectInputStream in;
+//		private ObjectOutputStream out;
+//
+//		public Receive(ObjectInputStream in, ObjectOutputStream out) {
+//			this.in = in;
+//			this.out = out;
+//		}
+//
+//		@Override
+//		public void run() {
+//			try {
+//				while (true) {
+//					ClientMessage msg = (ClientMessage) in.readObject();
+//					System.out.println("Resultado: " + msg.getX());
+//				}
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} catch (ClassNotFoundException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//
+//	}
 
 	public static void main(String[] args) {
-		new Client();
-		System.out.println("New Client Created!");
+		int nClients = 10;
+		for (int i = 0;i<nClients;i++){
+			new Client(i);
+			System.out.println("New Client Created!");
+		}
 	}
 
 }
--- TFD2015/src/clientSide/ClientState.java
+++ TFD2015/src/clientSide/ClientState.java
@@ -1,5 +1,7 @@
 package clientSide;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class ClientState {
@@ -7,4 +9,55 @@ public class ClientState {
 	private String ipAddress; 
 	private ArrayList<String> configuration;
 	private int view_number = 0;
+	
+	public ClientState(int id) {
+		this.id = id;
+		try {
+			this.ipAddress = InetAddress.getLocalHost().toString();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public ArrayList<String> getConfiguration() {
+		return configuration;
+	}
+
+	public void setConfiguration(ArrayList<String> configuration) {
+		this.configuration = configuration;
+	}
+
+	public int getView_number() {
+		return view_number;
+	}
+
+	public void setView_number(int view_number) {
+		this.view_number = view_number;
+	}
+	
+	public String toString(){
+		StringBuilder str = new StringBuilder();
+		str.append("ID: " + id + "\n");
+		str.append("IP: " + ipAddress + "\n");
+		str.append("View Number: " + view_number + "\n");
+		
+		return str.toString();
+	}
 }
